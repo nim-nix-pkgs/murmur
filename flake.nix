d2b2208713b4d81eb9d8488b7f585f30d6f1515c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:09 -0300

    chore: re index murmur

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."murmur-master".dir   = "master";
+  inputs."murmur-master".owner = "nim-nix-pkgs";
+  inputs."murmur-master".ref   = "master";
+  inputs."murmur-master".repo  = "murmur";
+  inputs."murmur-master".type  = "github";
+  inputs."murmur-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."murmur-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."murmur-v0_1_0".dir   = "v0_1_0";
+  inputs."murmur-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."murmur-v0_1_0".ref   = "master";
+  inputs."murmur-v0_1_0".repo  = "murmur";
+  inputs."murmur-v0_1_0".type  = "github";
+  inputs."murmur-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."murmur-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
